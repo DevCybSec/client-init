982e23ffab7861f52c9eb1e15da32ad22352b216
edgarmacias <edgarmacias@aragon.unam.mx>
2025-01-05 18:33:46 -0600

    Fix: Fixing errors howed when build

--- src/app/admin/login.tsx
+++ src/app/admin/login.tsx
@@ -2,11 +2,11 @@ export default function AdminLogin(){
     return  (
         <div className="flex items-center justify-center h-screen bg-gray-100">
             <div className="w-full max-w-sm p-6 bg-white rounded-lg shadow-md">
-                <h2 className="text-2xl font-bold text-center text-gray-700 mb-6">Login</h2>
+                <h2 className="text-2xl font-bold text-center text-gray-700 mb-6">Iniciar Sesi&oacute;n</h2>
                 <form>
                     <div className="mb-4">
                         <label htmlFor="email" className="block text-sm font-medium text-gray-600">
-                            Email
+                            Correo
                         </label>
                         <input
                             type="email"
@@ -30,13 +30,13 @@ export default function AdminLogin(){
                         type="submit"
                         className="w-full px-4 py-2 text-white bg-blue-500 rounded-lg hover:bg-blue-600 focus:ring focus:ring-blue-300 focus:outline-none"
                     >
-                        Login
+                        Iniciar Sesi&oacute;n
                     </button>
                 </form>
                 <p className="mt-4 text-sm text-center text-gray-600">
-                    Don't have an account?{' '}
+                    No tienes una cuenta?{' '}
                     <a href="#" className="text-blue-500 hover:underline">
-                        Sign up
+                        Registrate
                     </a>
                 </p>
             </div>
